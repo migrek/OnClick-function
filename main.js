057461057c9a8b2880882ce1b658cab3f91665b1
Ekaterina Grudinskaya <40333436+migrek@users.noreply.github.com>
2018-10-29 21:50:01 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-var tab = document.getElementsByClassName('item-pagination');
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready(){
+    let tab = document.getElementsByClassName('item-pagination');
+    let  tablist = document.getElementsByClassName('pagination');
+
+    tab.addEventListener('click', function(e) {
+        e.classList.add('active-tab');
+});
+}
+
+/*for (var i = 0; i < tab.length; i++) {
+    tab[i].addEventListener('click', function(event) {
+        event.classList.add('active-tab');
+    });
+}*/
+
+
+/*var tab = document.getElementsByClassName('item-pagination');
 var  tablist = document.getElementsByClassName('pagination');
 
 tab.addEventListener('click', addClass());
 
 function addClass(e) {
     e.target.classList.add('active-tab');
-};
+};*/
 //s[... tablist].indexOf
